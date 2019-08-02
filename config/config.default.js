a2f23a474d4803cc149fec73a47f38d5fe5c9533
wangguoliang <wangguoliang@itcast.cn>
2019-08-02 21:05:11 +0800

    Update config.default.js

--- config/config.default.js
+++ config/config.default.js
@@ -19,6 +19,13 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [ 'response', 'weixinbodyparser' ];
 
+  config.cluster = {
+      listen: {
+        path: '',
+        port: 7002,
+        hostname: '0.0.0.0',
+      }
+  };
   // add your user config here
   const userConfig = {
     myAppName: 'kids_node_server',
